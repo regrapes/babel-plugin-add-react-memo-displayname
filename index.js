a9826c668766e9b09c7c5d8ccf0041fffb8c8acf
Torsten <info@by-torsten.com>
2021-10-24 19:22:04 +0200

    chore: add prettier / linting

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-const SUPPORTED_HOCS = ['forwardRef', 'memo'];
+const SUPPORTED_HOCS = ['forwardRef', 'memo']
 
 const isAnonymousComponent = (t, callee) => {
-  
   if (t.isIdentifier(callee) && SUPPORTED_HOCS.includes(callee.name)) {
-    return true;
+    return true
   }
 
   if (t.isMemberExpression(callee)) {
-    const {
-      property
-    } = callee;
+    const { property } = callee
 
     if (t.isIdentifier(property) && SUPPORTED_HOCS.includes(property.name)) {
-      return true;
+      return true
     }
   }
 
-  return false;
-};
+  return false
+}
 
 module.exports = function transform({ types: t }) {
   return {
@@ -28,19 +25,28 @@ module.exports = function transform({ types: t }) {
         for (const declarator of declarations) {
           if (t.isIdentifier(declarator.node.id) && t.isCallExpression(declarator.node.init)) {
             let callExpression
-            
+
             if (t.isArrowFunctionExpression(declarator.node.init.arguments[0])) {
               callExpression = declarator.node.init
-            } else if (t.isCallExpression(declarator.node.init.arguments[0]) && isAnonymousComponent(t, declarator.node.init.callee) && t.isArrowFunctionExpression(declarator.node.init.arguments[0].arguments[0])) {
-              callExpression = declarator.node.init.arguments[0]
+            } else if (
+              t.isCallExpression(declarator.node.init.arguments[0]) &&
+              isAnonymousComponent(t, declarator.node.init.callee) &&
+              t.isArrowFunctionExpression(declarator.node.init.arguments[0].arguments[0])
+            ) {
+              ;[callExpression] = declarator.node.init.arguments
             }
 
             if (callExpression && isAnonymousComponent(t, callExpression.callee)) {
-              path.insertAfter(t.variableDeclarator(t.identifier(declarator.node.id.name + '.displayName'), t.stringLiteral(declarator.node.id.name)))
+              path.insertAfter(
+                t.variableDeclarator(
+                  t.identifier(`${declarator.node.id.name}.displayName`),
+                  t.stringLiteral(declarator.node.id.name)
+                )
+              )
             }
           }
         }
-      }
-    }
+      },
+    },
   }
 }
